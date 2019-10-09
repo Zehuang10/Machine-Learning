30cdcd1665252e7ea385c21b9176ca260ee99d4b
Zehuang Hong Chen <56319060+Zehuang10@users.noreply.github.com>
2019-10-09 11:41:13 -0700

    Create K-NN.py

---
+++ Classification-Models/K-NN.py
@@ -0,0 +1,15 @@
+#K-Nearest Neighbors Algorithm
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+## Get the Data
+** Read the 'KNN_Project_Data csv file into a dataframe **
+
+df = pd.read_csv('KNN_Project_Data')
+
+**Check the head of the dataframe.**
+
+df.head()
